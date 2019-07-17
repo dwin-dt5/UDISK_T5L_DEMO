24ab60b904723dddec1627da3f958cf84de67880
dwincxy <1638057926@qq.com>
2019-07-17 13:36:42 +0800

    update

--- user_projects/usb_demo/main/lib.h
+++ user_projects/usb_demo/main/lib.h
@@ -5,17 +5,17 @@
 #include "../../lib_projects/dwin_51/src/t5los8051.h"
 #include "../../lib_projects/dwin_51/src/sys.h"
 
-/*
+
 #include "../../lib_projects/dwin_dgus/src/dgus.h"
-*/
+
 /*
 #include "../../lib_projects/dwin_uart/src/uart.h"
 */
-/*
+
 #include "../../lib_projects/dwin_usb/src/app_interface.h"
 #include "../../lib_projects/dwin_usb/src/usb_dgus.h"
 #include "../../lib_projects/dwin_usb/src/para_port.h"
 #include "../../lib_projects/dwin_usb/src/file_sys.h"
-*/
+
 
 #endif
